d73c1a1b7d7150779eabe1c9ed4cddc0f6e562ec
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-04-25 23:25:43 +0530

    Added:Added splash screen to app

---
+++ app/src/main/java/com/example/loginapp/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.loginapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+    private var TIME_OUT:Long = 3000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        loadSplashScreen()
+    }
+    private fun loadSplashScreen(){
+        Handler().postDelayed({
+            // You can declare your desire activity here to open after finishing splash screen. Like MainActivity
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        },TIME_OUT)
+    }
+}
